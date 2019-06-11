fbde22ef938d37c38cbf5ddff3dc5c483c651fbb
chooeeh <chchgml10@gmail.com>
2019-06-11 18:14:19 +0900

    06/11 앱 아이콘 추가

---
+++ FragmentTEst/FragmentTEst/app/src/main/java/com/example/fragmenttest/Fragment5.java
@@ -0,0 +1,112 @@
+package com.example.fragmenttest;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/** 메이커스 칼리지 fragment 입니다. */
+public class Fragment5 extends Fragment implements View.OnClickListener{
+    View view;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager recyclerviewLayoutmanager;
+    private ArrayList<Data> data = new ArrayList<>();
+    private MyAdapter myAdapter;
+    private Intent intent;
+    private Dialog dialog;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_right2, container, false);
+        recyclerView = (RecyclerView)view.findViewById(R.id.recyclerview_right2);
+        recyclerView.setHasFixedSize(true);
+        recyclerviewLayoutmanager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(recyclerviewLayoutmanager);
+        myAdapter = new MyAdapter(data,getContext());
+        myAdapter.setOnItemClickListener(this);
+        recyclerView.setAdapter(myAdapter);
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        data.clear();
+        data.add(new Data("메이커스칼리지","02-970-9250", "029709250"));
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int idx = recyclerView.getChildAdapterPosition(v);
+        Dialog(idx);
+        dialog.show();
+
+    }
+
+    /** 전화번호 복사 */
+    public void Copy(Context context, String string){
+        ClipboardManager clipboardManager = (ClipboardManager)context.getSystemService(context.CLIPBOARD_SERVICE);
+        ClipData clipData = ClipData.newPlainText("phoneNumber", string);
+        clipboardManager.setPrimaryClip(clipData);
+        Toast.makeText(getContext(), "복사되었습니다", Toast.LENGTH_SHORT).show();
+    }
+
+    public void Dialog(final int idx){
+        final String phoneNum = data.get(idx).getCall();
+        dialog = new Dialog(getContext());
+        dialog.setContentView(R.layout.dialog_call);
+        TextView dialog_textview = (TextView)dialog.findViewById(R.id.dialog_text);
+        dialog_textview.setText(data.get(idx).getPhonenum());
+        Button dialog_btn1 = (Button)dialog.findViewById(R.id.dialog_btn1);
+        Button dialog_btn2 = (Button)dialog.findViewById(R.id.dialog_btn2);
+        dialog_textview.setText(data.get(idx).getPhonenum());
+        dialog_btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /** 통화 권한 확인 후, 통화 */
+                int permissionCheck = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE);
+                if(permissionCheck == PackageManager.PERMISSION_DENIED){
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE}, 0);
+                }else{
+                    intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse("tel:"+phoneNum));
+                    getContext().startActivity(intent);
+                }
+
+            }
+        });
+        dialog_btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Copy(getContext(), phoneNum);
+            }
+        });
+    }
+}
